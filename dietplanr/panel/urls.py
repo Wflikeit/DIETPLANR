8fee1144978d23f6450c8106043a1323fd2cf23e
avgprogrammer <88510480+avgprogrammer@users.noreply.github.com>
2023-09-15 23:54:59 +0200

    Making progress on drag and drop

--- dietplanr/panel/urls.py
+++ dietplanr/panel/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('appointment/<slug:dietitian_slug>/', CreateAppointmentView.as_view(), name='appointment'),
     path('account/register/', EditAccount.as_view(), name='registration'),
     path('clients/', ManageClientsView.as_view(), name='show_clients'),
-    path('login/', LoginView.as_view(), name='login'),  # Przekierowanie na AppLoginView dla logowania
+    path('login/', LoginView.as_view(), name='login'),
     path('account/logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('dietitians/<slug:dietitian_slug>', DisplayDietitianProfile.as_view(), name='show_clients'),
     path('calendar/', ManageCalendar.as_view(), name='show_calendar'),
