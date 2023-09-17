6c8ffaaf2c7b47e57c6d0f62a47f02f2badfc624
Wiktor Frątczak <117844405+Wflikeit@users.noreply.github.com>
2023-09-17 23:14:40 +0200

    finished the look of home view

--- dietplanr/panel/static/js/dietitian_panel_base.js
+++ dietplanr/panel/static/js/dietitian_panel_base.js
@@ -7,25 +7,26 @@ links.forEach(link => {
 const clientsActivitySpan = document.getElementsByClassName("clients-span")[0];
 const messagesSpan = document.getElementsByClassName("messages-span")[0];
 const appointmentsSpan = document.getElementsByClassName("appointments-span")[0];
-// const overviewWrappers = document.getElementsByClassName("overview-container")[0]
-//     .querySelectorAll('[class$="-wrapper"]');
-// console.log(overviewWrappers)
+
 fetch("http://127.0.0.1:8000/api/notifications/")
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        data.forEach((element)=>{
+        data.forEach((element) => {
             const span = document.createElement('span')
-            switch (element["type"]){
+            console.log(element['title']);
+            span.textContent = element['title'];
+            span.classList.add('message')
+            switch (element["type"]) {
                 case 'message':
-
-                    console.log(element['title']);
-                    span.textContent = element['title'];
-                    span.classList.add('message')
                     messagesSpan.appendChild(span);
                     break;
+                case 'activity':
+                    clientsActivitySpan.appendChild(span);
+                    break;
+                case 'appointment':
+                    appointmentsSpan.appendChild(span);
             }
-            // span
+
         })
     });
-// console.log(overviewWrappers)
