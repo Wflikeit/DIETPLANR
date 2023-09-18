928ef12b0350b8333b0cbc072139934b404a4e7b
Wiktor Frątczak <117844405+Wflikeit@users.noreply.github.com>
2023-09-18 23:35:44 +0200

    implemented usage of GET and PUT requests on frontend

--- dietplanr/panel/static/js/dietitian_panel_base.js
+++ dietplanr/panel/static/js/dietitian_panel_base.js
@@ -1,32 +1,32 @@
-const sidebar = document.querySelector("[data-sidebar]");
-const links = sidebar.querySelectorAll("[data-sidebar-link]");
-links.forEach(link => {
-    if (link.href == window.location.href) link.classList.add("active");
-});
-
-const clientsActivitySpan = document.getElementsByClassName("clients-span")[0];
-const messagesSpan = document.getElementsByClassName("messages-span")[0];
-const appointmentsSpan = document.getElementsByClassName("appointments-span")[0];
-
-fetch("http://127.0.0.1:8000/api/notifications/")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        data.forEach((element) => {
-            const span = document.createElement('span')
-            console.log(element['title']);
-            span.textContent = element['title'];
-            span.classList.add('message')
-            switch (element["type"]) {
-                case 'message':
-                    messagesSpan.appendChild(span);
-                    break;
-                case 'activity':
-                    clientsActivitySpan.appendChild(span);
-                    break;
-                case 'appointment':
-                    appointmentsSpan.appendChild(span);
-            }
-
-        })
-    });
+// const sidebar = document.querySelector("[data-sidebar]");
+// const links = sidebar.querySelectorAll("[data-sidebar-link]");
+// links.forEach(link => {
+//     if (link.href == window.location.href) link.classList.add("active");
+// });
+//
+// const clientsActivitySpan = document.getElementsByClassName("clients-span")[0];
+// const messagesSpan = document.getElementsByClassName("messages-span")[0];
+// const appointmentsSpan = document.getElementsByClassName("appointments-span")[0];
+//
+// fetch("http://127.0.0.1:8000/api/notifications/")
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//         data.forEach((element) => {
+//             const span = document.createElement('span')
+//             console.log(element['title']);
+//             span.textContent = element['title'];
+//             span.classList.add('message')
+//             switch (element["type"]) {
+//                 case 'message':
+//                     messagesSpan.appendChild(span);
+//                     break;
+//                 case 'activity':
+//                     clientsActivitySpan.appendChild(span);
+//                     break;
+//                 case 'appointment':
+//                     appointmentsSpan.appendChild(span);
+//             }
+//
+//         })
+//     });
